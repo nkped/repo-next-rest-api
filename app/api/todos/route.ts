bd61acffb34d12670590fa20a97178fefc88017d
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-07-18 17:13:22 +0200

    done

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -27,5 +27,49 @@ export async function DELETE(request: Request) {
         }   
 
      })
-     return NextResponse.json({"message":`Tode ${id} deleted.. `})
+     return NextResponse.json({"message":`Todo ${id} deleted.. `})
+}
+
+
+
+
+export async function POST(request: Request) {
+    const { userId, title }: Partial<Todo> = await request.json()
+
+     if (!userId || !title) return NextResponse.json({ "message": "did not recieve 'userId' and/or 'title'.."})
+
+     const res = await fetch(DATA_SOURCE_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "API_KEY": API_KEY
+        },
+        body: JSON.stringify({
+            userId, title, completed: false
+        })
+     })
+     const newTodo: Todo = await res.json()
+
+     return NextResponse.json(newTodo)
+}
+
+
+export async function PUT(request: Request) {
+    const { userId, id, title, completed }: Todo = await request.json()
+
+     if (!userId || !id || !title || typeof(completed) !== 'boolean') return NextResponse.json({ "message": "did not recieve 'userId' and/or 'title'.."})
+
+     const res = await fetch(`${DATA_SOURCE_URL}/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "API_KEY": API_KEY
+        },
+        body: JSON.stringify({
+            userId, title, completed: false
+        })
+     })
+     const updatedTodo: Todo = await res.json()
+
+     return NextResponse.json(updatedTodo)
 }
