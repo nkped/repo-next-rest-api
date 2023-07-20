e979a18f67fa6f5db43fc406350adbed63eea904
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-07-20 22:23:40 +0200

    add limiter

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,7 +1,22 @@
 import { NextResponse } from "next/server"
+import { limiter } from "../config/limiter"
 
+export async function GET(request: Request) {
+  const remaining = await limiter.removeTokens(1)
+  console.log('remaining:', remaining)
 
+  const origin = request.headers.get('origin')
 
-export async function GET() {
+  if (remaining < 0 ) {
+    return new NextResponse(null, {
+        status: 429,
+        statusText: 'Too many requests',
+        headers: {
+          'Access-Control-Allow-Origin': origin || '*',
+          'Content-Type': 'text/plain',
+        }
+
+    })
+  }
   return NextResponse.json({"message": "Hello, Next.js"})
 }
--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,32 @@
 import { NextResponse } from "next/server";
 
 
-export function middleware(request: Request) {
+//ifcon, regex and config object can all asign middleware as shown below function    
 
+
+export function middleware(request: Request) {
     console.log('Middleware')
     console.log(request.method)
     console.log(request.url)
-
-   const origin = request.headers.get('origin')
-   console.log(origin) 
-
-   return NextResponse.next()
+    
+    const origin = request.headers.get('origin')
+    console.log(origin) 
+    
+    return NextResponse.next()
 }
+
+//middleware to match (and only run on) all api-routes
+//config object can be used instead or together with ifcon  
+export const config = {
+    matcher: '/api/:path*'
+}
+
+/*  //put code inside funtion     
+    if (request.url.includes('/api/')) {
+        //put conditional code here
+    }
+*/
+
+    //regex
+    // const regex = new RegExp('/api/*')
+    //if ( regex.test(request.url)) { put exe code here }
