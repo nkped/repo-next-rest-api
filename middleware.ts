2c366fffcc0ffb1e7dc43adf31fe395e9aecc655
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-07-21 15:01:58 +0200

    done

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,29 @@
 import { NextResponse } from "next/server";
 
+const allowedOrigins = process.env.NODE_ENV === 'production' ? [ 'https://www.yoursite.com', 'https://yoursite.com'] : ['http://localhost:3000', 'https://www.google.com']
+
 
 //ifcon, regex and config object can all asign middleware as shown below function    
-
-
 export function middleware(request: Request) {
+    
+    const origin = request.headers.get('origin')
+    console.log(origin) 
+   
+    if (origin && !allowedOrigins.includes(origin)) {
+        return new NextResponse(null, {
+            status: 400,
+            statusText: 'Bad request',
+            headers: {
+                'Content-Type': 'text/plain',
+            }
+        })
+    }
+   
+   
     console.log('Middleware')
     console.log(request.method)
     console.log(request.url)
     
-    const origin = request.headers.get('origin')
-    console.log(origin) 
     
     return NextResponse.next()
 }
